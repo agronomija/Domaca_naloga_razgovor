5874c6a577b287536d09eade8f0333790b8ddcd4
Andraz Corel <serviranje@gmail.com>
2021-09-02 15:26:25 +0200

    manjši dodatki

--- funkcije.py
+++ funkcije.py
@@ -1,18 +1,26 @@
 import xml.etree.ElementTree as ET
 import requests
 
-def pretvorba(datum):
-    return datum[:4]
+def pretvorba(date):
+    """
+    Funkcija sprejme datum kot argument in vrne letnico
+    :param datum: string, v obliki '2007-09-01
+    :return: vrne letnico v stringu
+    """
+    return date[:4]
 
 
 def get_tecaji(year):
-    leto = pretvorba(year)
+    """
+
+    :param datum: string npr. 2007
+    :return: vrne dva slovarja (GBP, USD), datum (v obliki: '2007-09-09') kot ključe in tečaj na tisti dan kot vrednost
+    """
     url = 'https://bsi.si/_data/tecajnice/dtecbs-l.xml'
 
     resp = requests.get(url)  # posljemo request in dobimo vsebino strani, v tem primeru xml
     root = ET.fromstring(resp.content) #resp.content je string, zato uporabimo ET.fromstring (preberemo, razclenimo xml)
 
-    EUR = dict()
     GBP = dict()
     USD = dict()
 
@@ -20,7 +28,8 @@ def get_tecaji(year):
         cel_datum = tecajnica.attrib['datum'] #cel datum, ki ga dobimo iz attributa tecajnice v obliki '2007-09-01'
         datum = pretvorba(cel_datum)  # tukaj bomo preverili ali je datum pravi
 
-        if int(datum) == 2010:
+        if int(year) == int(datum): #ce sta si letnica, ki je vnesena kot atribut v funkcijo
+            # in letnica vsake posamezne tecajnice enaki, potem zapisemo v slovarja naslednje vrednosti in kljuce
             for tecaj in tecajnica:
                 if tecaj.get('oznaka') == 'USD':
                     #print('USD: ', tecaj.text)
@@ -31,8 +40,13 @@ def get_tecaji(year):
                     GBP[cel_datum] = tecaj.text
     return GBP, USD #vrnemo dva slovarja, v vsakem pod kljuci shranjeni datumi, pod vrednostjo pa vrednost valute na tisti dan
 
+#print(get_tecaji('2009'))
 
 def get_vse_valute():
+    """
+
+    :return: po abecedi urejen seznam vseh valut (oznak: USD), ki so objavljene v tecajnici na banki slovenije
+    """
     url = 'https://bsi.si/_data/tecajnice/dtecbs-l.xml'
 
     resp = requests.get(url)  # posljemo request in dobimo vsebino strani, v tem primeru xml
@@ -50,6 +64,10 @@ def get_vse_valute():
 
 
 def get_casovna_obdobja():
+    """
+
+    :return: vrne vse letnice, po katerih je banka slovenije beležila tečaje. urejen seznam po velikosti
+    """
     url = 'https://bsi.si/_data/tecajnice/dtecbs-l.xml'
 
     resp = requests.get(url)  # posljemo request in dobimo vsebino strani, v tem primeru xml
@@ -67,7 +85,14 @@ def get_casovna_obdobja():
 
 
 def get_tecaji_vsi(year, valuta):
-    leto = pretvorba(year)
+    """
+    Uporabimo, če nas zanima letno nihanje vrednosti tečaja za določeno valuto. izberemo leto in valuto
+    :param datum: string 2007-09-01
+    :param valuta: string kratice neke valute
+    :return: vrne slovar z datumi ('2007-09-09') kot ključi in vrednostjo tečaja kot vrednostjo,
+    za določeno leto, ki smo ga določili v atributu year
+    """
+    leto = pretvorba(year) #iz attributa datum izluščimo letnico
     url = 'https://bsi.si/_data/tecajnice/dtecbs-l.xml'
 
     resp = requests.get(url)  # posljemo request in dobimo vsebino strani, v tem primeru xml
@@ -80,7 +105,7 @@ def get_tecaji_vsi(year, valuta):
         cel_datum = tecajnica.attrib['datum'] #cel datum, ki ga dobimo iz attributa tecajnice v obliki '2007-09-01'
         datum = pretvorba(cel_datum)  # tukaj bomo preverili ali je datum pravi
 
-        if int(datum) == 2010:
+        if int(datum) == int(year):
             for tecaj in tecajnica:
                 if tecaj.get('oznaka') == valuta:
                     #print(f'{valuta} ', tecaj.text)
@@ -89,4 +114,29 @@ def get_tecaji_vsi(year, valuta):
 
     return EUR #vrnemo en slovar z datumi kot ključi in vrednostmi kot vrednosti valute
 
-print(sorted(get_tecaji_vsi('2007', 'USD').keys()))
+#print(sorted(get_tecaji_vsi('2007', 'HRK').keys()))
+
+x = get_tecaji_vsi('2007', 'HRK')
+print(x)
+
+print('HRK za datum 2007-04-04: ', x.get('2007-04-04'))
+
+
+def spremeni_letnico_datuma(year, seznam_datumov):
+    """
+
+    :param year: npr string '2007'
+    :param seznam_datumov: seznam datumov v taki obliki: '2007-10-10'
+    :return: vrne nov seznam istih datumov samo da imajo letnico tako kot smo si jo izbrali (year)
+    """
+    nov_seznam = []
+    for datum in seznam_datumov:
+        datum = year + datum[4:]
+        nov_seznam.append(datum)
+    return nov_seznam
+
+"""
+x = '2000-10-10'
+x = '1999' + x[4:]
+print(x)
+"""
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from funkcije import get_vse_valute, get_casovna_obdobja, get_tecaji, pretvorba
+from funkcije import get_vse_valute, get_casovna_obdobja, get_tecaji, pretvorba, get_tecaji_vsi, spremeni_letnico_datuma
 import requests
 
 app = Flask(__name__)
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def index():
     vse_valute = get_vse_valute()
     letnice = get_casovna_obdobja()
+
     return render_template('izberi_valuto.html', vse_valute=vse_valute, letnice=letnice)
 
 
@@ -16,12 +17,20 @@ def index():
 @app.route("/valuta", methods=['POST'])
 def valuta():
     if request.method == 'POST':
-        ime_valute1 = request.form.get('valuta1')
+        ime_valute1 = request.form.get('valuta1') #iz forme preberemo prvo valuto
         ime_valute2 = request.form.get('valuta2')
-        leto = request.form.get('leto')
+        leto = request.form.get('leto') #iz forme preberemo leto
 
+        obdobje = ['2009-03-03', '2009-03-04', '2009-03-05']
+        novo_obdobje = spremeni_letnico_datuma(leto, obdobje) #spremeni obdobje na leto pridobljeno iz forme
+        print(novo_obdobje)
+        # za primer bomo prikazali obdobje od '2009-03-03'-'2009-03-05'
+        x1 = get_tecaji_vsi(leto, ime_valute1) #dobimo slovar vseh tecajev, za doloceno leto za doloceno valuto pridobljeno iz forme
+        x2 = get_tecaji_vsi(leto, ime_valute2)
 
-        return render_template('valuta.html', ime_valute1=ime_valute1, ime_valute2=ime_valute2, leto=leto)
+        return render_template('valuta.html', ime_valute1=ime_valute1, ime_valute2=ime_valute2, leto=leto,
+                               novo_obdobje=novo_obdobje,
+                               x1=x1, x2=x2)
 
 
 
